f4c0faad4297cc70b8aec82751b34137c7fb964a
citra-rahman <citrapuspita.r@gmail.com>
2024-08-01 16:57:44 +0700

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,64 @@
-import Image from "next/image";
+"use client";
+import { Box, Button, Toolbar, Typography } from "@mui/material";
+import { Gruppo } from "next/font/google";
+import Header from "./components/Header";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+export const gruppo = Gruppo({
+  subsets: ["latin"],
+  weight: '400'
+});
 export default function Home() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#fff',
+      },
+      secondary: {
+        main: '#45afa0',
+      }
+    },
+    typography: {
+      allVariants: {
+        fontFamily: gruppo.style.fontFamily,
+      },
+    }
+  });
   return (
-    <main className='flex flex-row justify-center items-center m-auto'>
-      <div className="m-2">
-        <h1 className="text-5xl font-bold italic">Hello I am Citra P Rahman</h1>
-        <span>I am a full-stack Developer based in Istanbul, Turkey.</span>
-      </div>
-      <Image alt='citra' src='/citra.jpg' width={500} height={500} className="m-2 rounded-full object-cover" />
-    </main>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <Header />
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: '15vmax'
+        }}>
+          <Toolbar />
+          <div>
+            <Typography variant="h6" sx={{ color: '#45afa0' }} gutterBottom>Hi, my name is </Typography>
+            <Typography sx={{
+              fontWeight: 'bold',
+              typography: { xs: 'h5', md: 'h3' }
+            }}
+            gutterBottom
+            >
+              Citra Puspita Rahman.</Typography>
+            <Typography
+              sx={{
+                typography: { xs: 'h5', md: 'h3' },
+                fontWeight: 'bold'
+              }}
+              gutterBottom>I build things for the web.</Typography>
+            <Typography component='div' sx={{ maxWidth: '50vmax', pb: 4 }}>
+              I am a software engineer specializing in building web application. Currently, I am living in Istanbul, Turkey. I am focusing on building applications with Ruby On Rails and or React JS.
+            </Typography>
+            <Button color='secondary' variant="outlined">Check out my Resume</Button>
+          </div>
+        </Box>
+      </main>
+    </ThemeProvider>
   )
 }
