a3688d4d6ef4e238c9823a647f38dbb061ebd04b
citra-rahman <citrapuspita.r@gmail.com>
2024-08-10 11:05:35 +0700

    update about description

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,7 +4,7 @@ export const about = {
   name: "Citra Puspita Rahman",
   headline: "I build things for the web",
   description:
-    " I am a software engineer specializing in building web application. Currently, I am living in Istanbul, Turkey. I am focusing on building applications with Ruby On Rails and or React JS.",
+    " I am a software engineer specializing in building web application. Currently, I am living in Istanbul, Turkey. I am focusing on building applications with Ruby On Rails, Node JS, React JS.",
   resumeButtonName: " Check out my Resume",
   resumeLink:
     "https://www.dropbox.com/scl/fi/mwgw35xvc6qbtyw9lk1tm/Citra-Resume.pdf?rlkey=1kp1ursb98i41b0d6eupwe6b9&st=6zs055x3&raw=1",
